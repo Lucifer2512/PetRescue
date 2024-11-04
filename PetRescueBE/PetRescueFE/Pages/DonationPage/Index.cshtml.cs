eccae1927270753d572ca04a7ab3da678acc38d6
Thang491 <dohongthang123@gmail.com>
2024-11-04 12:49:57 +0700

    fixconflix

---
+++ PetRescueBE/PetRescueFE/Pages/DonationPage/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DataAccessLayer.Context;
+using DataAccessLayer.Entity;
+
+namespace PetRescueFE.Pages.DonationPage
+{
+    public class IndexModel : PageModel
+    {
+
+        private readonly DataAccessLayer.Context.PetRescueDbContext _context;
+
+        public IndexModel(DataAccessLayer.Context.PetRescueDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Donation> Donation { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Donations != null)
+            {
+                Donation = await _context.Donations
+                .Include(d => d.Event)
+                .Include(d => d.Shelter)
+                .Include(d => d.User).ToListAsync();
+            }
+        }
+}
