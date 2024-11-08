aa26d96859adf85a9a102ceb13e4a51f1b36434c
duythuan123 <146064542+duythuan123@users.noreply.github.com>
2024-11-08 13:03:01 +0700

    Test

--- PetRescueBE/BusinessLayer/Service/Interface/IAdoptionApplicationService.cs
+++ PetRescueBE/BusinessLayer/Service/Interface/IAdoptionApplicationService.cs
@@ -1,11 +1,5 @@
 ﻿using BusinessLayer.Model.Request;
 using BusinessLayer.Model.Response;
-using BusinessLayer.Models.Response;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BusinessLayer.Service.Interface
 {
@@ -16,6 +10,5 @@ namespace BusinessLayer.Service.Interface
         Task<BaseResponseModel<AdoptionApplicationResponseModel>> AddAsync(AdoptionApplicationRequestModel request);
         Task<BaseResponseModel<AdoptionApplicationResponseModel>> UpdateAsync(AdoptionApplicationRequestModelForUpdate request, Guid id);
         Task<BaseResponseModel> DeleteAsync(Guid id);
-
     }
 }
